77c08a6b19c2d589d78f6c6be33663706dd1f1d1
manhong2112 <manhong2112@gmail.com>
2017-04-02 01:08:05 +0800

    add variable length args support

--- func.py
+++ func.py
@@ -12,35 +12,52 @@ class Func():
         self.runtime = 0
 
     def __call__(self, args, env, scope):
-        assert len(args) == self.args_len
+        # ((lambda (...) ...) 1)
+        assert len(args) >= self.args_len
         self.runtime += 1
         exec_scope = (self.runtime, self.closure)
         gc = GC()
-        varlist = []
-        for i in range(0, self.args_len):
-            if self.args_namelist[i][0] == "$":
-                self.args_namelist[i] = self.args_namelist[i][1:]
+        gc_namelist = []
+        args_vallist = []
+        args_namelist = self.args_namelist[:]
+        for i, name in enumerate(args_namelist):
+            fexpr = False
+            if name[0] == "$":
+                fexpr = True
+                name = name[1:]
+                args_namelist[i] = name
+            if fexpr:
                 val = args[i]
             else:
                 val, _gc = interp.interp0(args[i], env, scope)
                 gc.extend(_gc)
+            args_vallist.append(val)
+            gc_namelist.append(name)
+        if self.args_len > 0 and name == "...":
+            varargs = [args_vallist.pop()]
+            for index, eachargs in enumerate(args, i):
+                if fexpr:
+                    val = eachargs
+                else:
+                    val, _gc = interp.interp0(self.body, env, exec_scope)
+                    gc.extend(_gc)
+                varargs.append(val)
+            args_vallist.append(varargs)
+
+        for index, name in enumerate(args_namelist):
+            assert len(args_vallist) == len(args_namelist)
             env.define(
                 exec_scope, # scope
-                self.args_namelist[i], # var name
-                val) # value
-            varlist.append(self.args_namelist[i])
+                name, # var name
+                args_vallist[index]) # value
         val, _gc = interp.interp0(self.body, env, exec_scope)
         gc.extend(_gc)
-        # TODO scan to clear unclosured variable
         if isinstance(val, Func):
-            gc.addClosureGC(exec_scope, varlist)
+            gc.addClosureGC(exec_scope, gc_namelist)
         else:
             gc.cleanClosureGC(env)
-            gc.add(exec_scope, varlist)
-        # print("__call__")
-        # gc.printClosureGC()
+            gc.add(exec_scope, gc_namelist)
         return val, gc
-
     def __str__(self):
         return f"<Func {self.name}>"
 
