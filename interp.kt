be8da602daecfe028eadb3f989be4f6fe8969f6d
manhong2112 <manhong2112@gmail.com>
2017-04-01 14:01:34 +0800

    Fixed SyntaxError no matter if it can run, at least there is not syntax error in the code...

--- interp.kt
+++ interp.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 /*
  * 參照某天跟冰封提起的方法嘗試實現的一個解釋器
  * 用VSCode寫的...鬼知道能不能用(攤手
@@ -5,44 +7,55 @@
 
 // parser str to list, kotlin version
 class Interp() {
-    fun parser(expr: String): ArrayList<Any> {
+    class Value(val value: Any?)
+    class Quote : Stack<Any>()
+    fun value_parser(expr: String): Value {
+        return Value(null)
+    }
+    fun isQuote(expr: Any): Boolean {
+        return false
+    }
+
+    fun parser(expr: String): Stack<Any> {
         // TODO custom class to represent subtyping relationship
-        val res = arrayListOf<Any>()
-        val last = arrayListOf<Any>(res)
+        val res = Stack<Any>()
+        val last = Stack<Stack<Any>>()
+        last.push(res)
         val buffer = StringBuilder()
         var FLAG = 0
         val FLAG_DEFAULT = 0
         val FLAG_STRING = 1
         val FLAG_ESCAPING_STRING = 2
         val FLAG_COMMENT = 3
-        val ESCAPING_LIST = hashMapOf("n" to "\n", "\"" to "\"", "\\" to "\\")
+        val ESCAPING_LIST = hashMapOf('n' to '\n', '\"' to '\"', '\\' to '\\')
         var lineNum = 1
         var charNum = 0
         var index = -1
         val length = expr.length
+        loop@
         while(index < length - 1) {
             charNum += 1
             index += 1
             val char = expr[index]
             when(FLAG) {
                 FLAG_COMMENT -> {
-                    if(char == "\n") {
+                    if(char == '\n') {
                         FLAG = FLAG_DEFAULT
                     }
-                    continue
+                    continue@loop
                 }
                 FLAG_ESCAPING_STRING -> {
                     buffer.append(ESCAPING_LIST[char])
                     FLAG = FLAG_STRING
                     // TODO expected SyntaxError
-                    continue
+                    continue@loop
                 }
                 FLAG_STRING -> {
                     when(char) {
-                        "\\" -> {
+                        '\\' -> {
                             FLAG = FLAG_ESCAPING_STRING
                         }
-                        "\"" -> {
+                        '\"' -> {
                             FLAG = FLAG_DEFAULT
                             buffer.append('"')
                         }
@@ -50,71 +63,73 @@ class Interp() {
                             buffer.append(char)
                         }
                     }
-                    continue
+                    continue@loop
                 }
             }
             when(char) {
-                "(", "[" -> {
+                '(', '[' -> {
                     if(buffer.isNotEmpty()) {
-                        last.last().append(value_parser(buffer))
-                        buffer.length = 0
+                        last.last().add(value_parser(buffer.toString()))
+                        buffer.setLength(0)
                     }
-                    if(last.last().isQuote()) {
+                    if(isQuote(last.last())) {
                         val new = Quote()
-                        last.last().append(new)
-                        last.append(new)
+                        last.last().add(new)
+                        last.push(new)
                     } else {
-                        val new = arrayListOf<Any>()
-                        last.last().append(new)
-                        last.append(new)
+                        val new = Stack<Any>()
+                        last.last().add(new)
+                        last.push(new)
                     }
                 }
-                ")", "]" -> {
+                ')', ']' -> {
                     val l = last.pop()
-                    if buffer.isNotEmpty() {
-                        l.append(value_parser(buffer))
-                        buffer.length = 0
+                    if(buffer.isNotEmpty()) {
+                        l.push(value_parser(buffer.toString()))
+                        buffer.setLength(0)
                     }
                 }
-                " " -> {
-                    if buffer.isNotEmpty() {
-                        l.append(value_parser(buffer))
-                        buffer.length = 0
+                ' ' -> {
+                    val l = last.pop()
+                    if(buffer.isNotEmpty()) {
+                        l.push(value_parser(buffer.toString()))
+                        buffer.setLength(0)
                     }
                 }
-                "\n" -> {
+                '\n' -> {
+                    val l = last.pop()
                     lineNum += 1
                     charNum = 0
-                    if buffer.isNotEmpty() {
-                        l.append(value_parser(buffer))
-                        buffer.length = 0
+                    if(buffer.isNotEmpty()){
+                        l.push(value_parser(buffer.toString()))
+                        buffer.setLength(0)
                     }
                 }
-                "'" -> {
+                '\''-> {
                     if (index + 1 >= length ||
-                        (expr[index + 1] != "(" && expr[index + 1] != "[")) {
+                        (expr[index + 1] != '(' && expr[index + 1] != '[')) {
                         // SyntaxError(f"""{expr.split(endl)[lineNum-1]}\n{'-' * (charNum - 1 + 13)}^""")
                         throw Error()
                     }
                     index += 1
-                    new = Quote()
-                    last.last().append(new)
-                    last.append(new)
+                    val new = Quote()
+                    last.last().push(new)
+                    last.push(new)
                 }
-                "," -> {
-                    if (!last.last().isQuote()) {
+                ',' -> {
+                    if (!isQuote(last.last())) {
                         // raise SyntaxError(f"""{expr.split(endl)[lineNum-1]}\n{'-' * (charNum - 1 + 13)}^""")
                     }
                     if (index + 1 >= length ||
-                        (expr[index + 1] != "(" && expr[index + 1] != "[") ) {
+                        (expr[index + 1] != '(' && expr[index + 1] != '[') ) {
 //                        raise SyntaxError(f"""{expr.split(endl)[lineNum-1]}\n{'-' * (charNum - 1 + 13)}^""")
                     }
                     index += 1
-                    new = []
-                    last.last().append(new)
-                    last.append(new)
+                    val new = Stack<Any>()
+                    last.last().push(new)
+                    last.push(new)
                 }
-                "\"" -> {
+                '"' -> {
                     if (FLAG == FLAG_DEFAULT) {
                         FLAG = FLAG_STRING
                         buffer.append('"')
@@ -123,7 +138,7 @@ class Interp() {
                         // TODO raise SyntaxError(f"""{expr.split(endl)[lineNum-1]}\n{'-' * (charNum - 1 + 13)}^""")
                     }
                 }
-                "\\" -> {
+                '\\' -> {
                     if (FLAG == FLAG_DEFAULT) {
                         // raise SyntaxError(f"""{expr.split(endl)[lineNum-1]}\n{'-' * (charNum - 1 + 13)}^""")
                         // 
@@ -131,7 +146,7 @@ class Interp() {
                         // ???
                     }
                 }
-                ";" -> {
+                ';' -> {
                     FLAG = FLAG_COMMENT
                 }
                 else -> {
@@ -143,7 +158,7 @@ class Interp() {
             // raise SyntaxError
         }
         if (buffer.isNotEmpty()) {
-            last.last().append(value_parser(buffer))
+            last.last().push(value_parser(buffer.toString()))
         }
         return res
     }
